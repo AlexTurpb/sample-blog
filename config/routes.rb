1ac72c4749d66d6be3e9e4a7e95f5f56168b6e44
AlexTurpb <turpb41@gmail.com>
2018-04-28 18:29:31 +0300

    transfer params from contacts to index, with hard-coded routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'home/contacts'
   get 'home/index'
+  post 'home/contacts'
+  post 'home/index'
 
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
