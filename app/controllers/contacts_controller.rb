ab6239b92566a29267352e22f6cece425b20bced
AlexTurpb <turpb41@gmail.com>
2018-04-29 14:57:54 +0300

    save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,15 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new params_contact
+    @contact.save
+  end
+
+  private
+
+  def params_contact
+    params.require(:contact).permit(:email, :message)
   end
 end
+
+
