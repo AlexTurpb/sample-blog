e657039e76b75e05c9d3561ab6d2e928e88a3d86
AlexTurpb <turpb41@gmail.com>
2018-04-30 15:14:02 +0300

    - old code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   
   def index
-    render plain: params[:contacts].inspect
   end
     
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   
   get 'home/index'
-<<<<<<< HEAD
-  
-=======
 
->>>>>>> origin/variant_with_controller
   resource :contacts, only: [:new, :create] 
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
