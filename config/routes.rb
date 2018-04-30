a3fd615be7a67c730c67d74d9b14e03a8ef88b5a
AlexTurpb <turpb41@gmail.com>
2018-04-30 15:02:20 +0300

    delete coflict routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   get 'home/contacts'
   get 'home/index'
+<<<<<<< Updated upstream
   post 'home/contacts'
   post 'home/index'
 
+=======
+  
+  resource :contacts, only: [:new, :create] 
+>>>>>>> Stashed changes
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
