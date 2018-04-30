56cbeec57c1abfe44dcc3d5b387eba068f29e8a1
AlexTurpb <turpb41@gmail.com>
2018-04-30 19:40:56 +0300

    new articles save to db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,5 +4,14 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new params_article
+    @article.save
   end
+
+  private
+
+  def params_article
+    params.require(:article).permit(:title, :text)
+  end
+
 end
