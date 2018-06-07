6da03a1e0d048b7f3f948a0f758a5f636f93fca8
AlexTurpb <turpb41@gmail.com>
2018-06-07 23:38:28 +0300

    Allow to peremit username in prarameters for sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   add_flash_types :danger, :info, :warning, :success, :primary, :secondary, :dark
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
